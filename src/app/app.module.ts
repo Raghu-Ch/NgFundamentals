7a9ab0bef21475c1c485e7736693afa0f9bf662d
Raghu-Ch <raghu6004@gmail.com>
2018-01-17 23:54:49 -0500

    Add Materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroParentComponent } from './hero-parent/hero-parent.component';
 import { HeroChildComponent } from './hero-child/hero-child.component';
-import { VoteComponent } from './vote/vote.component';
-import { VotetakerComponent } from './votetaker/votetaker.component';
 
+import { MzSidenavModule } from 'ng2-materialize';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroParentComponent,
-    HeroChildComponent,
-    VoteComponent,
-    VotetakerComponent
+    HeroChildComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MzSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
